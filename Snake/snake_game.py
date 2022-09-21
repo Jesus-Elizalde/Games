463026cc58f191007f52c3a243af41e593a023ff
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-09-20 21:36:59 -0700

    py snake

--- Snake/snake_game.py
+++ Snake/snake_game.py
@@ -2,8 +2,9 @@ import pygame
 import random
 from enum import Enum
 from collections import namedtuple
-pygame.init()
 
+pygame.init()
+font = pygame.font.SysFont("arial",25)
 class Direction(Enum):
     RIGHT = 1
     LEFT = 2
@@ -12,7 +13,16 @@ class Direction(Enum):
 
 Point = namedtuple("Point","x, y")
 
+# RGB colors
+WHITE = (225,225,225)
+RED = (200,0,0)
+BLUE1 = (0,0,225)
+BLUE2 = (0,100,225)
+BLACK = (0,0,0)
+
 BLOCK_SIZE = 20
+SPEED = 40
+
 
 class SnakeGame:
 
@@ -55,10 +65,25 @@ class SnakeGame:
         # 4. palce new food or just move
 
         # 5. update ui and clock
-
+        self._update_ui()
+        self.clock.tick(SPEED)
         # return game over and score
         game_over = False
         return game_over, self.score
+
+    def _update_ui(self):
+        self.display.fill(BLACK)
+
+        for pt in self.snake:
+            pygame.draw.rect(self.display, BLUE1,pygame.Rect(pt.x,pt.y,BLOCK_SIZE,BLOCK_SIZE))
+            pygame.draw.rect(self.display, BLUE2,pygame.Rect(pt.x+4,pt.y+4,12,12))
+
+        pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x,self.food.y,BLOCK_SIZE,BLOCK_SIZE))
+
+        text = font.render("Score: "+str(self.score),True,WHITE)
+        self.display.blit(text,[0,0])
+        pygame.display.flip()
+
 
 
 if __name__ == "__main__":
